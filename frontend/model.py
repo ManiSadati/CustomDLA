2d90e9fa1c7394633bbfd3b72da2c3905105f1fb
Manisadati <sadati.man@outlook.com>
2025-12-26 21:17:16 -0800

    successfully extract information for each graph node

--- frontend/model.py
+++ frontend/model.py
@@ -17,8 +17,7 @@ class MLP(nn.Module):
     def forward(self, x):
         x = self.stack(x)
         y = nn.functional.sigmoid(x)
-        z = nn.functional.sigmoid(x*-1)
-        return y+z
+        return y
 
 model = MLP()
 torch.save(model, PATH)
@@ -32,24 +31,13 @@ graph = traced.graph
 test_input = torch.randn([1,10])
 out_golden = model(test_input)
 out_actual = traced(test_input)
-print(out_actual - out_golden)
+assert(torch.max(out_actual - out_golden ).item() < 1e-9) #checking if the golden model and traced graph output the same values
 
 print(traced.graph)
 for n in traced.graph.nodes:
     print(n.name, n.op, n.target, n.args) # n.meta contains everything you need
 
-ShapeProp(traced).propagate(test_input)
-
-print("-------")
-for x in traced.graph.nodes:
-    tm = x.meta.get("tensor_meta")
-    mod = "-"
-    if  x.op == "call_module":
-        mod = traced.get_submodule(x.target)
-    print(x.name, tm.shape, tm.dtype, mod)
-
-print("*****")
-print([x for x,y in traced.named_parameters()])
+ShapeProp(traced).propagate(test_input) #adding shapes to each tensor's meta data
 
 class IRNode():
     def __init__(self): 
@@ -58,54 +46,65 @@ class IRNode():
         self.dtype = ""
         self.shape = ""
         self.op_name = ""
+        self.target = ""
+        self.output = ""
         self.inputs = []
         self.parameters = []
-        self.output = ""
     def print(self):
         print(f"""
-            name = {self.name}, \n
-            obj_type = {self.obj_type}, \n
-            dtype = {self.dtype}, \n
-            shape = {self.shape}, \n
-            op_name = {self.op_name}, \n
-            inputs = {self.inputs}, \n
-            parameters = {self.parameters}, \n
-            output = {self.output}, \n """
+            name = {self.name}
+            obj_type = {self.obj_type}
+            dtype = {self.dtype}
+            shape = {self.shape}
+            op_name = {self.op_name}
+            target = {self.target}
+            output = {self.output}
+            inputs = {self.inputs}
+            parameters = {self.parameters} """
         )
         
 IR_nodes = [] # each entry is an IRNode (name, op_type, op_name, type, [inputs], [parameters], output)
 
 def transform_graph(traced, IR_nodes):
     for n in traced.graph.nodes:
+
+        assert(n.meta.get("tensor_meta") != None)
         tm = n.meta.get("tensor_meta")
         new_node = IRNode()
         new_node.name = n.name
         new_node.obj_type = n.op
         new_node.dtype = tm.dtype
         new_node.shape = torch.tensor(tm.shape).tolist()
+        new_node.target = n.target
+        new_node.output = n.name
+        new_node.inputs = list(n.args)
 
+        # searching for op_name and parameters:
         if n.op == "placeholder":
             new_node.op_name = "input"
         if n.op == "call_method":
-            new_node.op_name = n.target
+            if n.target == "sigmoid":
+                new_node.op_name = n.target
+                print(type(n.target))
+            else:
+                new_node.op_name = ""
+        if n.op == "call_function":
+            new_node.op_name = ""
+            # new_node.op_name = n.target
         if n.op == "output":
-            new_node.op_name = n.target
+            new_node.op_name = "return"
         if n.op == "call_module":
             mod = traced.get_submodule(n.target)
             if isinstance(mod, nn.Linear):
-                new_node.op_name = "Linear"
+                new_node.op_name = "linear"
             elif isinstance(mod, nn.ReLU):
-                new_node.op_name = "ReLU"
+                new_node.op_name = "relu"
             else:
                 new_node.op_name = ""
-            tmp_parameters = [[x[0],torch.tensor(x[1].shape).tolist()] for x in mod.named_parameters()]
+            tmp_parameters = [[new_node.target+"."+x[0],torch.tensor(x[1].shape).tolist()] for x in mod.named_parameters()]
             new_node.parameters = tmp_parameters
-            
-        new_node.inputs = list(n.args)
-        new_node.output = n.name
-        
 
-
+        assert(new_node.op_name != "")
         IR_nodes.append(new_node)
     return 
 
